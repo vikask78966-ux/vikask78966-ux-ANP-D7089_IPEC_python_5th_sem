489044cae92752ee836a8a2d32480668d0b38156
Vikas kumar <vikask78966@gmail.com>
2026-06-25 17:21:15 +0530

    Added new class work program

---
+++ Class work/Day 1_Arithmetic operators_24th_june/11.Speed.py
@@ -0,0 +1,12 @@
+# Speed Calculator
+#input disctance in kilometers.
+distance = float(input("Enter the distance in kilometers :"))
+#input time in hours.
+time = float(input ("Enter the time in hours :"))
+#calculate speed in kilometers per hour.
+speed = distance / time
+#output the speed  in kilometers per hour.
+print("The speed is :", speed, "kilometers per hour")
+  
+    
+    
